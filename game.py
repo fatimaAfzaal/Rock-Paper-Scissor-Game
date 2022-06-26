83cad514378f3612dcb9ab868e5f8175444e1ea6
Fatima Afzaal <99525339+fatimaAfzaal@users.noreply.github.com>
2022-06-25 23:55:20 -0800

    Add files via upload

---
+++ game.py
@@ -0,0 +1,43 @@
+import random
+
+u_wins = 0
+c_wins = 0
+
+options = ["rock", "paper", "scissors"]
+
+print ("Welcome to Rock/Paper/Scissors Game !!!!!")
+while True:
+    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+    if user_input == "q":
+        break
+
+    if user_input not in options:
+        continue
+
+    random_number = random.randint(0, 2)
+    # rock: 0, paper: 1, scissors: 2
+    computer_pick = options[random_number]
+    print("Computer picked", computer_pick + ".")
+
+    if user_input == "rock" and computer_pick == "scissors":
+        print("You won!")
+        u_wins += 1
+
+    elif user_input == "paper" and computer_pick == "rock":
+        print("You won!")
+        u_wins += 1
+
+    elif user_input == "scissors" and computer_pick == "paper":
+        print("You won!")
+        u_wins += 1
+        
+    elif user_input == computer_pick:
+        print("Game draw!!!")
+
+    else:
+        print("You lost!")
+        c_wins += 1
+
+print("You won", u_wins, "times.")
+print("The computer won", c_wins, "times.")
+print("Thank you!!!!")
